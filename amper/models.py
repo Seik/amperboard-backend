575663b77f2b2ae0790bf0eae90d7f037c05492e
Iván Martín <ivanmartinga@gmail.com>
2017-04-29 11:04:29 +0200

    New models

--- amper/models.py
+++ amper/models.py
@@ -6,3 +6,23 @@ class Item(models.Model):
     consumption = models.PositiveIntegerField()
 
 
+class NasaReport(models.Model):
+    date = models.DateField()
+
+
+class Report(models.Model):
+    item = models.ForeignKey("Item")
+    start_time = models.TimeField()
+    duration = models.PositiveIntegerField(help_text="in minutes")
+    consumption = models.PositiveIntegerField(blank=True, null=True)
+
+
+class CapacityHour(models.Model):
+    hour = models.TimeField()
+    capacity = models.PositiveIntegerField()
+
+
+class Day(models.Model):
+    date = models.DateField(unique=True)
+    reports = models.ManyToManyField("Report")
+    capacity_hours = models.ManyToManyField("CapacityHour")
