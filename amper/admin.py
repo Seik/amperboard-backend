6d00168dc30085e30925bc7128faacd6b36dd72b
Iván Martín <ivanmartinga@gmail.com>
2017-04-29 11:05:59 +0200

    Add models to admin

--- amper/admin.py
+++ amper/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from amper.models import Item
+from amper.models import Item, Report, CapacityHour, Day
 
 admin.site.register(Item, admin.ModelAdmin)
+admin.site.register(Report, admin.ModelAdmin)
+admin.site.register(CapacityHour, admin.ModelAdmin)
+admin.site.register(Day, admin.ModelAdmin)
